961587125e7da512edc4afaa3a1973b266aca0b0
alvaroun <alvaromachadoferreira@hotmail.com>
2022-02-20 23:45:37 -0300

    Co-authored-by: Isis Ferreira  <IsisFer@users.noreply.github.com> Co-authored-by: Egysella <Egysella@users.noreply.github.com> Co-authored-by: tikaoo <tikaoo@users.noreply.github.com> Co-authored-by: Igorbacellar <Igorbacellar@users.noreply.github.com> Co-authored-by: Giovane A. Tiburcio <giovaner10@users.noreply.github.com> Co-authored-by: LuizEduardoLeite <LuizEduardoLeite@users.noreply.github.com> Co-authored-by: igorantonio22@outlook.com <igorantonio22@outlook.com>

---
+++ scripts/carrossel.js
@@ -0,0 +1,51 @@
+/* concluido, nao alterar */
+
+let time = 3000,
+    currentImageIndex = 0,
+    
+    images_C = document.
+            querySelectorAll("#slider-c img")
+
+    images_B = document.
+            querySelectorAll("#slider-b img")
+
+    images_A = document.
+            querySelectorAll("#slider-a img")
+
+    max = 2;
+
+function nextImage() {
+
+    images_C[currentImageIndex]
+        .classList.remove("selected")
+
+    images_B[currentImageIndex]
+        .classList.remove("selected")
+
+    images_A[currentImageIndex]
+        .classList.remove("selected")
+
+    currentImageIndex++
+
+    if(currentImageIndex >= max)
+        currentImageIndex = 0
+
+    images_C[currentImageIndex]
+        .classList.add("selected")
+
+    images_B[currentImageIndex]
+            .classList.add("selected")
+
+    images_A[currentImageIndex]
+        .classList.add("selected")
+
+}
+
+function start() {
+    setInterval(() => {
+        // troca de image
+        nextImage()
+    }, time)
+}
+
+window.addEventListener("load", start)
